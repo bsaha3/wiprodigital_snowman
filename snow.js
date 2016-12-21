c3fdb14240514229184c97de08747f83cb910a3e
Biswajit Saha <saha.biswajit.1992@gmail.com>
2016-12-21 18:34:03 +0530

    updated version 21/12

--- snow.js
+++ snow.js
@@ -1,6 +1,99 @@
+
+var k=0;
 //===========Dragging Hat===========================================
 
-  $(function() { $(".drag-image").draggable(); });
+
+  $('#hat1').click(function(){
+     $('#hat1').show().addClass('animated fadeOutUp');
+     setTimeout(function(){$('#hat1').css('display','none');},1000);
+     $('#hat2').css('display','block');
+     $('#hat2').show().addClass('animated fadeInLeft');
+     k++;
+     final();
+  });
+
+$('#scarf1').click(function(){
+     
+     $('#scarf1').css('display','none');
+     $('#scarf2').css('display','block');
+     $('#scarf2').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#nose1').click(function(){
+     
+     $('#nose1').css('display','none');
+     $('#nose2').css('display','block');
+     $('#nose2').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#hand11').click(function(){
+     
+     $('#hand11').css('display','none');
+     $('#hand12').css('display','block');
+     $('#hand12').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#hand21').click(function(){
+     
+     $('#hand21').css('display','none');
+     $('#hand22').css('display','block');
+     $('#hand22').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#eye11').click(function(){
+     
+     $('#eye11').css('display','none');
+     $('#eye12').css('display','block');
+     $('#eye12').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#eye21').click(function(){
+     
+     $('#eye21').css('display','none');
+     $('#eye22').css('display','block');
+     $('#eye22').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#btn11').click(function(){
+     
+     $('#btn11').css('display','none');
+     $('#btn12').css('display','block');
+     $('#btn12').show().addClass('animated swing');
+     k++;
+     final();
+  });
+
+  $('#btn21').click(function(){
+     
+     $('#btn21').css('display','none');
+     $('#btn22').css('display','block');
+     $('#btn22').show().addClass('animated swing');
+     k++;
+     final();
+  });
+  
+  function final(){
+    if(k==9){         
+
+          setTimeout(function(){ 
+            $("#tree").fadeIn(1000);
+            $("#greet").fadeIn(1000);
+            $("#inst").css('display','none');
+          }, 1000);   
+        }
+  }
 
 //===========Dragging Hat===========================================
 
@@ -20,112 +113,46 @@
 
     }
 
+    $('#i').keydown(function(event) {
+      // enter has keyCode = 13, change it if you want to use another button
+      if (event.keyCode == 13) {
+        text = document.getElementById("i").value;
+      $("#land").css('display','none');
+      $("#landbtn").css('display','none');
+      $("#i").css('display','none');
+      $("#text1").text("Hello "+text.toUpperCase());
+
+      setTimeout(function(){ 
+        $("#body-large-border").css('display','block');
+      }, 1000);
+      }
+    });
+
 //===========Welcome================================================
 
 
 //=================Snow man fill====================================
 
+$('#medium1').click(function(){
+     
+     $('#medium1').css('display','none');
+     $('#medium').css('display','block');
+     $('#medium').show().addClass('animated swing');
+  });
 
-
-var i=0;
-document.body.onkeyup=function(e){
-  
-    if(e.keyCode==32)
-    {
-      if(i==0)
-      {
-        $("#body-large").css('display','block');
-        setTimeout(function(){ 
-          $("#body-medium-border").css('display','block');
-        }, 1000);
-      }
-      if(i==1)
-      {
-        $("#body-medium").css('display','block');
-        setTimeout(function(){ 
-          $("#body-small-border").css('display','block');
-        }, 1000);
-      }
-      if(i==2)
-      {
-        $("#body-small").css('display','block');
-        
-        setTimeout(function(){ 
-          $(".parts").css('display','block');
-        }, 1000);
-      }
-      
-      i++;
-    }
-}
+  $('#small1').click(function(){
+     
+     $('#small1').css('display','none');
+     $('#small').css('display','block');
+     $('#small').show().addClass('animated swing');
+  });
 
 //=================Snow man fill====================================
 
-//===========Text===================================================
-        // setTimeout(function() {
-        // $("#text").fadeOut().empty();
-        // }, 2000);
-//===========Text===================================================
-
 
 //============End Greeting==========================================
 
-        var i=0;
-        function a(){         
-
-          setTimeout(function(){ 
-            $("#tree").fadeIn(1000);
-            $("#greet").fadeIn(1000);
-            $("#inst").css('display','none');
-          }, 1000);   
-        }
+        console.log(k);
+        
 
 //============End Greeting==========================================
-
-
-//=================Draggable elements===============================
-
-  var selectedElement = 0;
-  var currentX = 0;
-  var currentY = 0;
-  var currentMatrix = 0;
-
-  function selectElement(evt) {
-    selectedElement = evt.target;
-    currentX = evt.clientX;
-    currentY = evt.clientY;
-
-    currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');     
-
-      for(var i=0; i<currentMatrix.length; i++) {
-        currentMatrix[i] = parseFloat(currentMatrix[i]);
-      }
-    document.onmousemove = moveElement;
-  }
-
-  function moveElement(evt){
-  dx = evt.clientX - currentX;
-  dy = evt.clientY - currentY;
-  currentMatrix[4] += dx;
-  currentMatrix[5] += dy;
-  newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-
-  selectedElement.setAttributeNS(null, "transform", newMatrix);
-  currentX = evt.clientX;
-  currentY = evt.clientY;
-}
-
-function deselectElement(evt){
-  if(selectedElement != 0){
-    document.onmousemove = null;
-    document.onmouseout = null;
-    document.onmouseup = null;
-    selectedElement = 0;
-  }
-}
-
-selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)");
-selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");
-
-//=================Draggable elements===============================
-
